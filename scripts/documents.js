07eb4c460b896af2f08de4f35927dbd929ea56ad
Janet Suryadi <janet@suryadi.id.au>
2023-06-19 09:21:21 +1000

    Creating a file class

--- scripts/documents.js
+++ scripts/documents.js
@@ -5,8 +5,35 @@
 
 /*----------  Variable Declaration  ----------*/
 
-let files = document.querySelectorAll('.documents-modal__file');
+class file {
 
+    fileName = '';
+    fileContents = '';
+
+    constructor(newName, newContents) {
+        fileName = newName;
+        fileContents = newContents;
+    }
+
+    get fileName() {
+        return this.name;
+    }
+    set fileName(value) {
+        if (typeof value === 'string' && value.length > 0) {
+            this.fileName = value;
+        }
+    }
+    get fileContents() {
+        return this.fileContents;
+    }
+    set fileContents(value) {
+        this.fileContents = value;
+    }
+}
+// let file1 = new File('test1.txt', 'This is a test file');
+
+let files = document.querySelectorAll('.documents-modal__file');
+let fileContents = [];
 
 /*----------  Script assignment  ----------*/
 files.forEach(file => {
