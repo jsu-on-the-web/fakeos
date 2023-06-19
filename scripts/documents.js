0ca204ce444309988c2c3d19d39b9e29ec38be07
Janet Suryadi <janet@suryadi.id.au>
2023-06-19 20:57:50 +1000

    Got the file display update function

--- scripts/documents.js
+++ scripts/documents.js
@@ -1,6 +1,30 @@
 /*----------  Function Declaration  ----------*/
 
+const updateFileDisplay = () => {
+    filesContentList.forEach(file => {
+        // Spawn and add a file icon to the display
 
+        /* =============== The format of the file object =================
+            <div class="documents-modal__file">
+                                <img class="documents-modal__icon" src="/img/message_file.ico">
+                                <p class="documents-modal__file-name">dummy.txt</p>
+                            </div>
+        */
+        let fileToPlace = document.createElement('div');
+        fileToPlace.className = 'documents-modal__file';
+        let icon = document.createElement('img');
+        icon.className = 'documents-modal__icon';
+        icon.src = '../img/message_file.ico';
+        fileToPlace.appendChild(icon);
+        let fileName = document.createElement('p');
+        fileName.className = 'documents-modal__file-name';
+        const fileNameText = document.createTextNode(file.fileName);
+        fileName.appendChild(fileNameText);
+        fileToPlace.appendChild(fileName);
+        documentsDisplayList.appendChild(fileToPlace);
+
+    })
+}
 
 
 /*----------  Variable Declaration  ----------*/
@@ -30,12 +54,18 @@ class file {
         this.fileContents = value;
     }
 }
-// let file1 = new File('test1.txt', 'This is a test file');
+let testFile = new file('test1.txt', 'This is a test file');
 
 let files = document.querySelectorAll('.documents-modal__file');
-let fileContents = [];
+let documentsDisplayList = document.querySelector('.documents-modal__display-file-list');
+let filesContentList = [testFile];
 
 /*----------  Script assignment  ----------*/
+
+// We gotta display something in the display before the user makes a note in the notepad modal
+updateFileDisplay();
+
+
 files.forEach(file => {
     file.addEventListener('click', (event) => {
         console.log(`${event.target} was clicked`)
