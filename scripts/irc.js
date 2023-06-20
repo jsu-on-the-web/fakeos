ae74cead273d7daca396c2940cf1214b1d8b825e
Janet Suryadi <janet@suryadi.id.au>
2023-06-20 11:39:19 +1000

    IRC.js start

---
+++ scripts/irc.js
@@ -0,0 +1,21 @@
+/*----------  Function Declaration  ----------*/
+
+
+
+
+/*----------  Variable Declaration  ----------*/
+
+let memberNameList = ["windeaux9x", "DardiveN", "rKstorTiCKsuN", "CLIpANoWSTe", "rChAR", "ladja1982", "Xx_oneou_xX"]
+let memberList = document.querySelector(".irc-modal__member-list");
+
+
+/*----------  Script assignment  ----------*/
+
+memberNameList.forEach(name => {
+    let nameToPush = document.createElement("li");
+    nameToPush.className = "irc-modal__member-name";
+    nameToPush.appendChild(document.createTextNode(name));
+    memberList.appendChild(nameToPush);
+});
+
+
